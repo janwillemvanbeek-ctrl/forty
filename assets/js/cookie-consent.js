be920bdc2726288e89d9f42015ca1b01b41e347f
Jan-Willem van Beek <jan-willemvanbeek@MacBook-Air-5.local>
2026-03-13 14:19:49 +0100

    Harden Snitcher activation after cookie accept

--- assets/js/cookie-consent.js
+++ assets/js/cookie-consent.js
@@ -68,11 +68,28 @@
   }
 
   function loadSnitcher() {
-    if (window.Snitcher && window.Snitcher._loaded) {
+    function activateSnitcher() {
+      if (!window.Snitcher) return;
       try {
-        window.Snitcher.giveCookieConsent();
-        window.Snitcher.page();
+        if (typeof window.Snitcher.giveCookieConsent === 'function') {
+          window.Snitcher.giveCookieConsent(true);
+          window.Snitcher.giveCookieConsent();
+        }
       } catch (e) {}
+      try {
+        if (typeof window.Snitcher.page === 'function') window.Snitcher.page();
+        if (typeof window.Snitcher.pageview === 'function') window.Snitcher.pageview();
+      } catch (e) {}
+    }
+
+    function scheduleActivation() {
+      [0, 400, 1200, 3000].forEach(function (delay) {
+        setTimeout(activateSnitcher, delay);
+      });
+    }
+
+    if (window.Snitcher && window.Snitcher._loaded) {
+      scheduleActivation();
       return;
     }
     !function(e){"use strict";var t=e&&e.namespace;if(t&&e.profileId&&e.cdn){var i=window[t];if(i&&Array.isArray(i)||(i=window[t]=[]),!i.initialized&&!i._loaded)if(i._loaded)console&&console.warn("[Radar] Duplicate initialization attempted");else{i._loaded=!0;["track","page","identify","group","alias","ready","debug","on","off","once","trackClick","trackSubmit","trackLink","trackForm","pageview","screen","reset","register","setAnonymousId","addSourceMiddleware","addIntegrationMiddleware","addDestinationMiddleware","giveCookieConsent"].forEach((function(e){var a;i[e]=(a=e,function(){var e=window[t];if(e.initialized)return e[a].apply(e,arguments);var i=[].slice.call(arguments);return i.unshift(a),e.push(i),e})})),-1===e.apiEndpoint.indexOf("http")&&(e.apiEndpoint="https://"+e.apiEndpoint),i.bootstrap=function(){var t,i=document.createElement("script");i.async=!0,i.type="text/javascript",i.id="__radar__",i.setAttribute("data-settings",JSON.stringify(e)),i.src=[-1!==(t=e.cdn).indexOf("http")?"":"https://",t,"/releases/latest/radar.min.js"].join("");var a=document.scripts[0];a.parentNode.insertBefore(i,a)},i.bootstrap()}}else"undefined"!=typeof console&&console.error("[Radar] Configuration incomplete")}({
@@ -83,11 +100,9 @@
     });
     if (window.Snitcher && typeof window.Snitcher.ready === 'function') {
       window.Snitcher.ready(function () {
-        try {
-          window.Snitcher.giveCookieConsent();
-          window.Snitcher.page();
-        } catch (e) {}
+        scheduleActivation();
       });
     }
+    scheduleActivation();
   }
 })();
